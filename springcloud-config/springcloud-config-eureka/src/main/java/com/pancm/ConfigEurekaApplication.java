aa80649ca71d647a152635e6018eccbd9a925152
master <master@putaoduo.com>
2021-05-01 12:00:49 +0800

    update

--- springcloud-config/springcloud-config-eureka/src/main/java/com/pancm/ConfigEurekaApplication.java
+++ springcloud-config/springcloud-config-eureka/src/main/java/com/pancm/ConfigEurekaApplication.java
@@ -3,6 +3,9 @@ package com.pancm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * 
@@ -22,4 +25,12 @@ public class ConfigEurekaApplication {
 		SpringApplication.run(ConfigEurekaApplication.class, args);
 		 System.out.println("config 注册中心服务启动...");
 	}
+//	@EnableWebSecurity
+//	static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//		@Override
+//		protected void configure(HttpSecurity http) throws Exception {
+//			http.csrf().disable();
+//			super.configure(http);
+//		}
+//	}
 }
---
+++ springcloud-config/springcloud-config-eureka/src/main/java/com/pancm/SecurityConfig.java
@@ -0,0 +1,47 @@
+//package com.pancm;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.annotation.Order;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//
+//@Configuration
+//@EnableWebSecurity
+//@Order(1)
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Autowired
+//    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication().withUser("discUser").password("discPassword").roles("SYSTEM");
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//
+//        http .csrf() .disable() .authorizeRequests() .anyRequest().authenticated() .and() .httpBasic();
+//
+////        http .csrf() .disable() .requestMatchers().antMatchers("/eureka/**").and().authorizeRequests().antMatchers("/eureka/**").hasRole("SYSTEM").anyRequest().denyAll().and().httpBasic().and().csrf()
+////                .disable();
+//    }
+//
+////    @Configuration
+////    // no order tag means this is the last security filter to be evaluated
+////    public static class AdminSecurityConfig extends WebSecurityConfigurerAdapter {
+////
+////        @Autowired
+////        public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+////            auth.inMemoryAuthentication();
+////        }
+////
+////        @Override
+////        protected void configure(HttpSecurity http) throws Exception {
+////            http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER).and().httpBasic().disable().authorizeRequests().antMatchers(HttpMethod.GET, "/").hasRole("ADMIN").antMatchers("/info", "/health").authenticated().anyRequest().denyAll()
+////                    .and().csrf().disable();
+////        }
+////    }
+//}
